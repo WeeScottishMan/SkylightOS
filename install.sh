29bd3c79a3934bd049068ac8d0b9d225f3f8dcb8
WeeScottishMan <85060818+WeeScottishMan@users.noreply.github.com>
2026-06-11 19:36:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -Syu
 
-sudo pacman -S niri nautilus fuzzel mako waybar xdg-desktop-portal-gnome swaybg kitty polkit-gnome vesktop steam heroic-games-launcher-bin helium-browser-bin proton-cachyos tela-circle-icon-theme-green
+sudo pacman -S niri nautilus gnome-text-editor loupe showtime fuzzel mako waybar xdg-desktop-portal-gnome swaybg kitty polkit-gnome vesktop steam heroic-games-launcher-bin helium-browser-bin proton-cachyos tela-circle-icon-theme-green
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gtk.gtk4.Settings.FileChooser sort-directories-first true
